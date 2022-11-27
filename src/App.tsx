366fc28a81a7b486e917a7ad1792bd1ef02527ac
RedHeadphone <huzaifa.k@somaiya.edu>
2022-11-27 23:40:35 +0530

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,53 @@ import { Send } from './routes/Send';
 
 import './App.css';
 
-export const App = () => {
-	return (
-		<Switch>
-			<Route path="/profile">
-				<Profile />
-			</Route>
-			<Route path="/send">
-				<Send />
-			</Route>
-			<Route path="/">
-				<Home />
-			</Route>
-		</Switch>
-	);
+class App extends React.Component {
+
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			privateKey: null,
+			privateKeyEncrypted: true,
+			login: false,
+			loading: true,
+		}
+	}
+
+	privateKeyCheck(): boolean {
+		// chrome.storage.local.get('privateKey', function (result) {
+		// 	console.log('Value currently is ' + result);
+		// 	if (result.privateKey == null) {
+		// 		return false;
+		// 	}
+		// 	else {
+		// 		return true;
+		// 	}
+		// });
+		return false;
+	}
+
+	componentDidMount(): void { // set loading to false, checks private key exists (if not create wallet page), if it does, load wallet page
+		// console.log("App mounted");
+		// if (!this.privateKeyCheck()) {
+		// 	console.log("No private key found");
+		// }
+
+	}
+
+	render() {
+		return (
+			<Switch>
+				<Route path="/profile">
+					<Profile />
+				</Route>
+				<Route path="/send">
+					<Send />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		);}
 };
+
+export default App;
